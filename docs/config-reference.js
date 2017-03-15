5b6b35fcdb1470c2c57074a156a6a5cd029194ac
Rob Bayliss <rob@lastcallmedia.com>
2017-03-15 10:54:20 -0400

    Update the reference config

--- docs/config-reference.js
+++ docs/config-reference.js
@@ -1,114 +1,73 @@
 (function() {
 
+  // Set up some common variables here that will be reused below.
+  var baseUrl = 'http://localhost:8888';
+  var phpCheck = "**.php";
+  var jsCheck = "**.js"
+
   /**
    * This is the sample configuration file for these tasks.
    *
-   * Defaults are given here.  You don't need to define all (or any) of these
-   * properties if you don't need to override the defaults.
+   * Configure your tools here.  If any sections are omitted, the tasks associated
+   * with them will be skipped.  For example, if you don't add a `backstopjs`
+   * section, test:backstop will not be available.
    */
   var config = {
     // The config version.  Right now there's only 1.
-    version: 1,
+    "version": 1,
 
-    // The base URL of the server to use for BackstopJS and Phantomas tests.
-    // It will be passed to your scripts as an environment variable.
-    baseUrl: undefined,
-
-    // The directory containing a bower.json file, to be installed during the
-    // install:bower task.
-    bowerJsonDirectory: undefined,
-
-    // A gulp glob array or string pointing to the PHP files you want to check
-    // during check:phpcs and check:phplint.
-    phpCheck: [],
-
-    // The PHPCS standard to use during check:phpcs.  This is a filepath to the
-    // standard's XML file.
-    phpcsStandard: undefined,
-
-    // A gulp glob array or string pointing to the JS files you want to check
-    // during check:eslint.
-    jsCheck: [],
-
-    // The default for this object is empty, but the options are shown below.
-    behat: {
-      // bin: 'vendor/bin/behat',
-      // configFile: '/some/behat.yml',
+    "bower": {
+      "src": "./bower.json"
     },
-
-    // The default for this object is empty, but the options are shown below.
-    backstopjs: {
-      // src: 'path/to/backstop/backstop.js',
-      // junitGlob: 'path/to/backstop/**.xml',
-      // artifactGlob: 'path/to/backstop/reports/**'
+    "composer": {
+      "src": "./composer.json"
     },
-
-    phantomas: {
-      bin: './node_modules/.bin/phantomas',
-      // src: 'path/to/phantomas/config.yml'
+    "phpcs": {
+      // src is a gulp glob
+      "src": phpCheck
     },
-
-    // The default for this object is empty, but the options are shown below.
-    // `scss` takes an object describing the packs of SCSS you want to compile.
-    // `theme` and `libs` here are arbitrary identifiers.  You can use whatever
-    // names you want.
-    scss: {
-    //   theme: {
-    //     src: [],
-    //     dest: null,
-    //     maps: false,
-    //     min: true,
-    //     concat: false,
-    //     prefix: { browsers: 'last 2 versions' },
-    //     sassOptions: {},
-    //   },
-    //   libs: {
-    //     src: [],
-    //     dest: null,
-    //     maps: false,
-    //     min: true,
-    //     concat: false,
-    //     prefix: { browsers: 'last 2 versions' },
-    //     sassOptions: {},
-    //   }
+    "phplint": {
+      // src is a gulp glob
+      "src": phpCheck
     },
-
-    // The default for this object is empty, but the options are shown below.
-    // `js` takes an object describing the packs of JS you want to compile.
-    // `theme` and `libs` here are arbitrary identifiers.  You can use whatever
-    // names you want.
-    js: {
-      //   theme: {
-      //     src: [],
-      //     dest: null,
-      //     concat: false,
-      //     maps: './',
-      //     min: true
-      //   },
-      //   libs: {
-      //     src: [],
-      //     dest: null,
-      //     concat: false,
-      //     maps: './',
-      //     min: true
-      //   },
+    "eslint": {
+      // src is a gulp glob
+      "src": jsCheck
     },
-
-    // The default for this object is empty, but the options are shown below.
-    // `js` takes an object describing the packs of files you want to copy on
-    // build:copy. `theme` and `libs` here are arbitrary identifiers.  You can
-    // use whatever names you want.
-    copy: {
-      // theme: {
-      //   src: [],
-      //   dest: null,
-      //   imagemin: false,
-      // },
-      // libs: {
-      //   src: [],
-      //   dest: null,
-      //   imagemin: false,
-      // }
+    "behat": {
+      "configFile": "behat.yml"
+    },
+    "backstopjs": {
+      "src": "backstop/backstop.js",
+      "baseUrl": baseUrl,
+      "artifactGlob": "backstop/reports/**",
+      "junitGlob": "backstop/**.xml"
+    },
+    "phantomas": {
+      "src": "phantomas/phantomas.yml",
+      "baseUrl": baseUrl
+    },
+    "scss": {
+      "theme": {
+        // src is a gulp glob
+        "src": "*.scss",
+        "dest": "dist/",
+        "minify": true,
+      }
+    },
+    "js": {
+      "theme": {
+        "src": "*.js",
+        "dest": "dist/",
+        "minify": true,
+      }
+    },
+    "copy": {
+      "theme": {
+        "src": "*.jpg",
+        "imagemin": true,
+        "dest": "dist/"
+      }
     }
   };
 
