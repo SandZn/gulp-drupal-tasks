71917ce053367869b1e5e0017836d241fc77440b
Rob Bayliss <rob@lastcallmedia.com>
2017-03-25 18:12:06 -0400

    Button up PHPUnit tests

--- fixtures/test_package/GoodTest.php
+++ fixtures/test_package/GoodTest.php
@@ -5,4 +5,11 @@ class GoodTest extends \PHPUnit_Framework_TestCase {
   public function testIt() {
     $this->assertTrue(TRUE);
   }
+
+  /**
+   * @group grouped
+   */
+  public function testGroup() {
+    $this->assertTrue(TRUE);
+  }
 }
--- test/test/phpunit.js
+++ test/test/phpunit.js
@@ -1,20 +1,21 @@
 
 var path = require('path');
 var factory = require('../../lib/test/phpunit');
-var which = require('which');
-var expect = require('chai').expect;
 var PluginError = require('gulp-util').PluginError;
+var rimraf = require('rimraf');
+
+var chai = require('chai');
+var chaiFiles = require('chai-files');
+chai.use(chaiFiles);
+var expect = chai.expect;
+var file = chaiFiles.file;
 
 var inpath = path.join(__dirname, '../../fixtures');
+var outpath = path.join(__dirname, '../../out-fixtures');
 
 describe('PHPUnit Task', function() {
-  var phpunitBin;
-  before(function(done) {
-    which('phpunit', function(err, resolvedPath) {
-      phpunitBin = resolvedPath;
-      done(err);
-    });
-  });
+  beforeEach(rimraf.bind(null, outpath, {}));
+  afterEach(rimraf.bind(null, outpath, {}));
 
   it('Should use the default config', function() {
     var configs = [undefined, {}];
@@ -33,27 +34,22 @@ describe('PHPUnit Task', function() {
     });
   });
 
-  it('Should fail on an invalid config or opts being passed', function() {
-    expect(factory.bind(factory, '')).to.throw(PluginError, 'config must be an object');
-    expect(factory.bind(factory, {}, '')).to.throw(PluginError, 'opts must be an object');
-  });
+  var invalidConfigTests = [
+    { it: 'Should fail on invalid config', config: '', message: 'config must be an object' },
+    { it: 'Should fail on invalid opts', opts: '', message: 'opts must be an object' },
+    { it: 'Should fail on an invalid bin', config: { bin: {} }, message: 'bin must be a string' },
+    { it: 'Should fail on an invalid src', config: { src: {} }, message: 'src must be a string' },
+    { it: 'Should fail on an invalid baseurl', config: { baseUrl: {} }, message: 'baseUrl must be a string' },
+    { it: 'Should fail on an invalid baseurl from opts', opts: { baseUrl: {} }, message: 'baseUrl must be a string' },
+    { it: 'Should fail on an invalid testsuite', config: { testsuite: {} }, message: 'testsuite must be a string' },
+    { it: 'Should fail on an invalid group', config: { group: {}, message: 'group must be a string' } },
+  ];
 
-  var invalidConfigTests = {
-    'Should fail on an invalid src': {
-      config: { src: {} },
-      message: 'src must be a string',
-    },
-    'Should fail on an invalid suite': {
-      config: { suite: {} },
-      message: 'suite must be a string',
-    }
-  };
-
-  for (t in invalidConfigTests) {
-    it(t, function() {
-      expect(factory.bind(factory, invalidConfigTests[t].config, invalidConfigTests[t].opts)).to.throw(PluginError, invalidConfigTests[t].message);
+  invalidConfigTests.forEach(function(test) {
+    it(test.it, function() {
+      expect(factory.bind(null, test.config, test.opts)).to.throw(PluginError, test.message);
     });
-  }
+  });
 
   it('Should not modify the config or opts object', function() {
     var cfg = Object.freeze({});
@@ -64,7 +60,7 @@ describe('PHPUnit Task', function() {
   it('Should run phpunit', function(done) {
     var stream = factory({
       src: path.join(inpath, 'phpunit.xml.dist'),
-      bin: phpunitBin,
+//      bin: phpunitBin,
       testsuite: 'passing'
     }, { silent: true })();
     stream.on('error', done);
@@ -72,10 +68,10 @@ describe('PHPUnit Task', function() {
     stream.resume();
   });
 
-  it('Should throw an error on phpunit failures', function(done) {
+  it('Should throw an error on an invalid phpunit bin', function(done) {
     var stream = factory({
       src: path.join(inpath, 'phpunit.xml.dist'),
-      bin: phpunitBin,
+      bin: '/some/nonexistent/path',
       suite: 'failing'
     }, { silent: true })();
     stream.on('error', function() {
@@ -87,17 +83,60 @@ describe('PHPUnit Task', function() {
     stream.resume();
   });
 
+  it('Should throw an error on phpunit failures', function(done) {
+    var stream = factory({
+      src: path.join(inpath, 'phpunit.xml.dist'),
+//      bin: phpunitBin,
+      suite: 'failing'
+    }, { silent: true })();
+    stream.on('error', function() {
+      done();
+    });
+    stream.on('end', function() {
+      throw new Error('Task did not fail.');
+    });
+    stream.resume();
+  });
+
+  it('Should pass the group through', function(done) {
+    var stream = factory({
+      src: path.join(inpath, 'phpunit.xml.dist'),
+//      bin: phpunitBin,
+      group: 'grouped',
+    }, { silent: true })();
+    stream.on('error', done);
+    stream.on('end', done);
+    stream.resume();
+  });
+
   it('Should pass baseUrl through', function(done) {
     var stream = factory({
       src: path.join(inpath, 'phpunit.xml.dist'),
-      bin: phpunitBin,
+//      bin: phpunitBin,
       suite: 'baseurl'
     }, {
-      silent: false,
+      silent: true,
       baseUrl: 'foo'
     })();
     stream.on('error', done);
     stream.on('end', done);
+    stream.resume();
+  });
+
+  it('Should generate junit output', function(done) {
+    var stream = factory({
+      src: path.join(inpath, 'phpunit.xml.dist'),
+//      bin: phpunitBin,
+      suite: 'passing'
+    }, {
+      silent: true,
+      junitDir: outpath
+    })();
+    stream.on('error', done);
+    stream.on('end', function() {
+      expect(file(path.join(outpath, 'phpunit.xml'))).to.exist;
+      done();
+    });
     stream.resume();
   });
 });
